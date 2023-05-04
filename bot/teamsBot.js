a9f83fc333e757396526c15db5aa3108c670bcdc
SiyaSalekar <salekarsiya77@gmail.com>
2023-05-04 11:47:37 +0100

    :coffin: delete unnecessary code

--- bot/teamsBot.js
+++ bot/teamsBot.js
@@ -13,34 +13,8 @@ class TeamsBot extends TeamsActivityHandler {
     this.likeCountObj = { likeCount: 0 };
 
     this.onMessage(async (context, next) => {
-      console.log("Running with Message Activity.");
-      let txt = context.activity.text;
-      const removedMentionText = TurnContext.removeRecipientMention(context.activity);
-      if (removedMentionText) {
-        // Remove the line break
-        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
-      }
-
-      // Trigger command by IM text
-      switch (txt) {
-        case "welcome": {
-          const card = cardTools.AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
-          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
-          break;
-        }
-        case "learn": {
-          this.likeCountObj.likeCount = 0;
-          const card = cardTools.AdaptiveCards.declare(rawLearnCard).render(this.likeCountObj);
-          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
-          break;
-        }
-        /**
-         * case "yourCommand": {
-         *   await context.sendActivity(`Add your response here!`);
-         *   break;
-         * }
-         */
-      }
+      
+      
 
       // By calling next() you ensure that the next BotHandler is run.
       await next();
